055cc0cb8b343278745dc7efbc635b24228bd65b
Daan De Meyer <daan.j.demeyer@gmail.com>
2018-04-21 22:58:37 +0200

    fix build.py on Windows

--- build.py
+++ build.py
@@ -2,8 +2,10 @@
 
 import subprocess
 import sys
+from os.path import join
 
 if __name__ == "__main__":
   OUT = 'out/cquery.vsix'
-  VSCE = 'node_modules/.bin/' + ('vsce.cmd' if sys.platform == 'win32' else 'vsce')
+  VSCE = 'vsce.cmd' if sys.platform == 'win32' else 'vsce'
+  VSCE = join('node_modules', '.bin', VSCE)
   sys.exit(subprocess.call([VSCE, 'package', '-o', OUT]))
