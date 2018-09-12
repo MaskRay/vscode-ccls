1221beab65c5b8ae66e00597a0360a05fc508fa1
Riatre Foo <foo@riat.re>
2018-09-13 03:25:53 +0800

    Use $ccls/inheritanceHierarchy to implement ccls.base

--- src/callHierarchy.ts
+++ src/callHierarchy.ts
@@ -1,7 +1,7 @@
-import { Event, EventEmitter, Location, TreeDataProvider, TreeItem, TreeItemCollapsibleState } from 'vscode';
+import { Event, EventEmitter, TreeDataProvider, TreeItem, TreeItemCollapsibleState } from 'vscode';
 import { LanguageClient } from 'vscode-languageclient/lib/main';
 import { parseUri } from './extension';
-
+import * as ls from 'vscode-languageserver-types';
 
 enum CallType {
   Normal = 0,
@@ -13,7 +13,7 @@ export class CallHierarchyNode {
   // These properties come directly from the language server.
   id: any
   name: string
-  location: Location
+  location: ls.Location
   callType: CallType
 
   // If |numChildren| != |children.length|, then the node has not been expanded
